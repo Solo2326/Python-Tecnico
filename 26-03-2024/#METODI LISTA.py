79c873956fa39c4daca052c9f48ffe1a61335d0d
Solo2326 <162106343+Solo2326@users.noreply.github.com>
2024-03-26 10:02:23 +0100

    Create #METODI LISTA.py

---
+++ 26-03-2024/#METODI LISTA.py
@@ -0,0 +1,10 @@
+#METODI LISTA
+
+nuova_lista = ['alessio','jacopo','Claudio','marco','simone']
+nuova_lista.append('nicola')
+nuova_lista.insert(7,'Alberto')
+bancatasx = ['alessandro','gabriele','riccardo','cristiano',]
+bancatasx.pop(2)
+nuova_lista.extend(bancatasx)
+bancatasx.sort()
+print(nuova_lista)
