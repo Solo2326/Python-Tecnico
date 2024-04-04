9f954fb2b7f01a49cdd14bf46871a09b4c4d74d5
Solo2326 <162106343+Solo2326@users.noreply.github.com>
2024-04-04 11:56:31 +0200

    Update filter().py

--- filter().py
+++ filter().py
@@ -6,3 +6,13 @@ numeri = [1,2,3,4,5,6,7,8,9,10]
 numeri_pari = list(filter(numero_pari, numeri))
 
 print(numeri_pari)
+
+
+
+
+
+
+def triplica(n):
+    return n * 3
+numeri_triplicati = list(map(triplica, numeri))
+print(numeri_triplicati)
