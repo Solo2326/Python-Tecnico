1c4afbb42cd7b8b2e32a873b4fc166074af5e92f
Solo2326 <162106343+Solo2326@users.noreply.github.com>
2024-04-11 09:50:56 +0200

    Create dizionario.py

---
+++ 11-04-2024/dizionario.py
@@ -0,0 +1,26 @@
+primo_dizionario = {
+
+    "brand":"Gucci",
+
+    #"colore":"nero",
+
+    "prezzo":"2000",
+
+    "taglie":['S','M','L'],
+
+    "colori":["nero","rosso","blu"]
+
+}
+
+
+
+
+primo_dizionario["colori"].append("verde")
+
+
+
+print("\n\n\---------------------------------------\n\n\n")
+print(primo_dizionario['colori'])
+print("\n\n\---------------------------------------\n\n\n")
+print(primo_dizionario['taglie'][1])
+print(primo_dizionario.keys)
